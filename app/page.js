6c90737a998a792995d82e7880f3365228885c35
Junaid Syed <sjunaid626@gmail.com>
2024-07-06 14:14:58 +0530

    Create navbar

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar";
+("./components/Navbar");
+
+export default function Page() {
+  return (
+    <div className="bg-primary h-96">
+      <Navbar />
+      <h1 className="text-white">My page</h1>
+    </div>
+  );
+}
