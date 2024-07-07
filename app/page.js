cb7ed8cf6b14f1b7083a76792a79f1ed5beca60d
Junaid Syed <sjunaid626@gmail.com>
2024-07-07 21:39:06 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import Navbar from "./components/navbar";
 import HomePage from "./pages/homepage";
 ("./components/Navbar");
+import CTA from "./pages/cta";
 
 export default function Page() {
   return (
     <div className="bg-primary">
       <Navbar />
       <HomePage />
-      <h1 className="text-white">My page</h1>
+      <CTA />
     </div>
   );
 }
