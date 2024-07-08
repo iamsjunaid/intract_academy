65b873a1d29aa478e0788d1dfaeb1266345c29ac
Junaid Syed <sjunaid626@gmail.com>
2024-07-08 12:06:50 +0530

    Update countdown.js

--- app/components/countdown.js
+++ app/components/countdown.js
@@ -1,6 +1,6 @@
 const CountdownCard = () => {
   return (
-    <div className="absolute top-[100rem] right-[34rem] bg-secondary text-white rounded-lg p-4 shadow-lg w-[18rem]">
+    <div className="absolute top-[100rem] right-[34rem] bg-secondary text-white rounded-lg p-4 shadow-lg w-[18rem] border">
       <div className="flex items-center space-x-2 mb-4">
         <svg
           className="w-5 h-5 text-orange-500"
