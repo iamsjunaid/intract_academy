167dbadc60fecfcaf177ad571da9ec7f16f0d2fa
Junaid Syed <sjunaid626@gmail.com>
2024-07-08 11:38:03 +0530

    Update cta.js

--- app/pages/cta.js
+++ app/pages/cta.js
@@ -6,13 +6,14 @@ import vertCardsData from "@/lib/vertCards";
 import VertCard from "../components/vertCard";
 import CountdownCard from "../components/countdown";
 import CommunityCard from "../components/communityCard";
+import RewardInfo from "../components/rewardInfo";
 
 export default function CTA() {
   const horiCards = horiCardsData;
   const vertCards = vertCardsData;
 
   return (
-    <main className="relative h-[400vh] w-full">
+    <main className="relative h-[600vh] w-full">
       <div className="absolute inset-0">
         {horiCards.map((card) => (
           <CardItem key={card.id} card={card} />
@@ -28,6 +29,14 @@ export default function CTA() {
       </div>
       <CountdownCard />
       <CommunityCard />
+      <div className="w-full border-red-400 absolute left-24">
+        <Image
+          src="/images/reward_info_path.png"
+          alt="reward_info_path"
+          fill
+        ></Image>
+      </div>
+      <RewardInfo />
     </main>
   );
 }
